99adbc96ce12575154a9d51cdfe3a51cea39ae57
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2024-12-20 14:35:16 +0100

    map tsx & png

---
+++ Image and map/trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="trees" tilewidth="50" tileheight="50" margin="100" tilecount="324" columns="18">
+ <image source="trees.png" width="1024" height="1024"/>
+</tileset>
