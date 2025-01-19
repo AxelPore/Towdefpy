65c8bbd819be0485e0eb9edad6f6617cbcc17490
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2025-01-19 13:36:32 +0100

    sendToTest

--- Image_and_map/Maps/Nexus.tsx
+++ Image_and_map/Maps/Nexus.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Nexus" tilewidth="30" tileheight="30" tilecount="1" columns="1">
+<tileset version="1.10" tiledversion="1.11.0" name="Nexus" tilewidth="50" tileheight="50" tilecount="1" columns="1">
  <image source="Nexus.png" width="50" height="50"/>
 </tileset>
--- Image_and_map/Maps/Rock.tsx
+++ Image_and_map/Maps/Rock.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Rock" tilewidth="30" tileheight="30" tilecount="1089" columns="33">
+<tileset version="1.10" tiledversion="1.11.0" name="Rock" tilewidth="50" tileheight="50" tilecount="400" columns="20">
  <image source="rockFloor.png" width="1000" height="1000"/>
 </tileset>
