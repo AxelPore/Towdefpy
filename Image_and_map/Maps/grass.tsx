984d38bd64b08e3c03f399bb419e191738bd448f
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2025-01-19 13:32:45 +0100

    sendToTest

--- Image_and_map/Maps/grass.tsx
+++ Image_and_map/Maps/grass.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="grass" tilewidth="40" tileheight="40" tilecount="100" columns="10">
+<tileset version="1.10" tiledversion="1.11.0" name="grass" tilewidth="50" tileheight="50" tilecount="64" columns="8">
  <image source="grass.png" width="420" height="420"/>
 </tileset>
