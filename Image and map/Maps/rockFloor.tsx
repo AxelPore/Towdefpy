42575d5a161866b53ce6d0e75b3f9ce139b7cad9
AxelPore <axel.pore@ynov.com>
2025-01-19 12:47:15 +0100

    commit

--- Image and map/Maps/rockFloor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="rockFloor" tilewidth="50" tileheight="50" tilecount="400" columns="20">
- <image source="rockFloor.png" width="1000" height="1000"/>
-</tileset>
